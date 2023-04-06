72f8e3fd6ec848f06b78d9d4d7fe9500a140562d
FedeLopez17 <fedelopez1712@gmail.com>
2023-04-06 17:09:22 -0300

    Add React custom component

--- src/index.js
+++ src/index.js
@@ -4,22 +4,24 @@ import logo from "./logo.svg";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const page = (
-  <section className="page">
-    <header>
-      <img src={logo} alt="React logo" width="300px"></img>
-    </header>
-    <main>
-      <h1>Fun facts about React:</h1>
-      <ul>
-        <li>Was first released in 2013.</li>
-        <li>Was originally created by Jordan Walke.</li>
-        <li>Has well over 100K stars on GitHub.</li>
-        <li>Is maintained by Facebook.</li>
-        <li>Powers thousands of enterprise apps, including mobile apps.</li>
-      </ul>
-    </main>
-  </section>
-);
+function Page() {
+  return (
+    <section className="page">
+      <header>
+        <img src={logo} alt="React logo" width="300px"></img>
+      </header>
+      <main>
+        <h1>Fun facts about React:</h1>
+        <ul>
+          <li>Was first released in 2013.</li>
+          <li>Was originally created by Jordan Walke.</li>
+          <li>Has well over 100K stars on GitHub.</li>
+          <li>Is maintained by Facebook.</li>
+          <li>Powers thousands of enterprise apps, including mobile apps.</li>
+        </ul>
+      </main>
+    </section>
+  );
+}
 
-root.render(page);
+root.render(<Page />);
