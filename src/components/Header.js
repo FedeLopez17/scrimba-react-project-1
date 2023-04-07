a998709e71c7b41fde0aca0a64f16296c2b33e98
FedeLopez17 <fedelopez1712@gmail.com>
2023-04-07 20:45:30 -0300

    Mainly improve styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,11 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <img src={logo} alt="React logo" width="300px"></img>
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <section className="logo-wrapper">
+          <img src={logo} alt="React logo"></img>
+          <h3>ReactFacts</h3>
+        </section>
+        <h4>React Course - Project 1</h4>
       </nav>
     </header>
   );
